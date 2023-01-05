99e72cc1a0d212a1080fa1f504d46eae9bf2651a
daniel.rdt <daniel.ruedt@campus.tu-berlin.de>
2023-01-05 15:31:30 +0100

    added training pipeline

--- bitcoin-sentiment-training-pipeline.py
+++ bitcoin-sentiment-training-pipeline.py
@@ -25,40 +25,52 @@ def g():
     from hsml.model_schema import ModelSchema
     import joblib
 
-    project = hopsworks.login()
+    project = hopsworks.login(api_key_value='U6PiDFwDVDQHP26X.XhXDZQ9QKiNwafhLh11PUntcyYW5Zp8aoXhoj1IJTGHDBu8owQJUKbFClHaehyMU')
     # get feature store from hopsworks
     fs = project.get_feature_store()
 
     # select or create feature view from feature group of titanic dataset in feature store
     try: 
-        feature_view = fs.get_feature_view(name="twitter_bitcoin_sentiment_model", version=1)
+        feature_view = fs.get_feature_view(name="twitter_bitcoin_sentiment", version=1)
     except:
-        twitter_fg = fs.get_feature_group(name="twitter_bitcoin_sentiment_model", version=1)
+        twitter_fg = fs.get_feature_group(name="twitter_bitcoin_sentiment", version=1)
         query = twitter_fg.select_all()
-        feature_view = fs.create_feature_view(name="twitter_bitcoin_sentiment_model",
+        feature_view = fs.create_feature_view(name="twitter_bitcoin_sentiment",
                                           version=1,
                                           description="Read from Twitter bitcoin sentiment dataset",
-                                          labels=["Bitcoin_Fluctuation_nextday"],
+                                          labels=["Bitcoin_fluctuation"],
                                           query=query)    
 
     # Read training data, randomly split into train/test sets of features (X) and labels (y) with 80/20 split      
     X_train, X_test, y_train, y_test = feature_view.train_test_split(0.2)
+    # transform labels into category and transform with label encoder from scikit learn
+    y_train.bitcoin_fluctuation = y_train.bitcoin_fluctuation.astype('category')
+    y_test.bitcoin_fluctuation = y_test.bitcoin_fluctuation.astype('category')
+    # label encoder and transform
+    from sklearn import preprocessing
+    le = preprocessing.LabelEncoder()
+    le.fit(['Bearish', 'Bullish'])
+    # print(list(le.classes_))
+    y_train_le = le.transform(y_train.values.ravel())
+    y_test_le = le.transform(y_test.values.ravel())
+
 
     # Train model with the Scikit-learn K-nearest-neighbors algorithm using our features (X_train) and labels (y_train)
-    model = KNeighborsClassifier(n_neighbors=2)
-    model.fit(X_train, y_train.values.ravel())
+    model = xgb.XGBClassifier(tree_method="hist", enable_categorical=True)
+    model.fit(X_train, y_train_le)
 
     # Evaluate model performance using the features from the test set (X_test)
     y_pred = model.predict(X_test)
 
     # Compare predictions (y_pred) with the labels in the test set (y_test)
-    metrics = classification_report(y_test, y_pred, output_dict=True)
-    results = confusion_matrix(y_test, y_pred)
-
+    metrics = classification_report(y_test, le.inverse_transform(y_pred), output_dict=True)
+    print(f"Accuracy: {metrics['accuracy']}")
+    
     # Create the confusion matrix as a figure
-    df_cm = pd.DataFrame(results, ['True Victim', 'True Survivor'],
-                         ['Pred Victim', 'Pred Survivor'])
-    cm = sns.heatmap(df_cm, annot=True)
+    results = confusion_matrix(y_test, le.inverse_transform(y_pred),labels=['Bearish', 'Bullish'])
+    df_cm = pd.DataFrame(results, ['True Bearish', 'True Bullish'],
+                            ['Pred Bearish', 'Pred Bullish'])
+    cm = sns.heatmap(df_cm, annot=True, fmt='g')
     fig = cm.get_figure()
 
     # Upload model to the Hopsworks Model Registry
@@ -66,30 +78,30 @@ def g():
     mr = project.get_model_registry()
     
     # The contents of the 'titanic_model' directory will be saved to the model registry. Create the dir, first.
-    model_dir="titanic_model"
+    model_dir="twitter_bitcoin_sentiment_model"
     if os.path.isdir(model_dir) == False:
         os.mkdir(model_dir)
 
     # Save both our model and the confusion matrix to 'model_dir', whose contents will be uploaded to the model registry
-    joblib.dump(model, model_dir + "/titanic_model.pkl")
+    joblib.dump(model, model_dir + "/twitter_bitcoin_sentiment_model.pkl")
     fig.savefig(model_dir + "/confusion_matrix.png")    
 
 
-    # Specify the schema of the model's input/output using the features (X_train) and labels (y_train)
+    # Specify the schema of the model's input/output using the features (X_train) and transformed labels (y_train_le)
     input_schema = Schema(X_train)
-    output_schema = Schema(y_train)
+    output_schema = Schema(y_train_le)
     model_schema = ModelSchema(input_schema, output_schema)
 
     # Create an entry in the model registry that includes the model's name, desc, metrics
-    titanic_model = mr.python.create_model(
-        name="titanic_modal", 
+    twitter_bitcoin_sentiment_model = mr.python.create_model(
+        name="twitter_bitcoin_sentiment", 
         metrics={"accuracy" : metrics['accuracy']},
         model_schema=model_schema,
-        description="Titanic Survivor Predictor"
+        description="Twitter Bitcoin Sentiment Predictor"
     )
     
     # Upload the model to the model registry, including all files in 'model_dir'
-    titanic_model.save(model_dir)
+    twitter_bitcoin_sentiment_model.save(model_dir)
     
 if __name__ == "__main__":
     if LOCAL == True :
