c2706af3987737cb457563d6768e57fc31a5be2b
daniel.rdt <daniel.ruedt@campus.tu-berlin.de>
2023-01-10 19:29:50 +0100

    added hyperparameter tuning

--- bitcoin-sentiment-training-pipeline.py
+++ bitcoin-sentiment-training-pipeline.py
@@ -38,7 +38,7 @@ def g():
         feature_view = fs.create_feature_view(name="twitter_bitcoin_sentiment",
                                           version=1,
                                           description="Read from Twitter bitcoin sentiment dataset",
-                                          labels=["Bitcoin_fluctuation"],
+                                          labels=["bitcoin_fluctuation"],
                                           query=query)    
 
     # Read training data, randomly split into train/test sets of features (X) and labels (y) with 80/20 split      
@@ -49,27 +49,54 @@ def g():
     # label encoder and transform
     from sklearn import preprocessing
     le = preprocessing.LabelEncoder()
-    le.fit(['Bearish', 'Bullish'])
+    le.fit(['Bearish', 'Bullish','Neutral'])
     # print(list(le.classes_))
     y_train_le = le.transform(y_train.values.ravel())
-    y_test_le = le.transform(y_test.values.ravel())
 
+    # Train model with the sklearn API of xgBoost algorithm using our features (X_train) and our encoded labels (y_train_le)
+    
+    # # First: Hyperparameter tuning using grid search cross validation
+    # from sklearn.model_selection import GridSearchCV
+    # # set ranges for hyperparamers 'learning_rate' (eta), 'max_depth', 'min_child_weight', 'n_estimators'
+    # grid_param = {
+    #     'learning_rate': [0.01,0.02,0.03,0.04,0.05,0.1,0.2], 
+    #     'max_depth': [3,4,5,6,7,8,9,10],
+    #     'min_child_weight': [0.5,1,2],
+    #     'n_estimators': [200,400,800,1000,1500]
+    # }
+    # # initialize grid search cv object
+    # gd_sr = GridSearchCV(estimator=xgb.XGBClassifier(tree_method="hist", objective="multi:softmax"),
+    #                  param_grid=grid_param,
+    #                  scoring='accuracy',
+    #                  cv=5,
+    #                  n_jobs=-1)
+    # # run grid seatch on training data
+    # gd_sr.fit(X_train,y_train_le)
+    # # check best parameters and resultung accuracy
+    # best_parameters = gd_sr.best_params_
+    # print(best_parameters)
+    # best_result = gd_sr.best_score_
+    # print(best_result)
+    # # resulting best parameters are: {'learning_rate': 0.01, 'max_depth': 3, 'min_child_weight': 1, 'n_estimators': 200} with a cv accuracy score of 0.561
 
-    # Train model with the Scikit-learn K-nearest-neighbors algorithm using our features (X_train) and labels (y_train)
-    model = xgb.XGBClassifier(tree_method="hist", enable_categorical=True)
+    # Now train model with found hyperparameters
+    model = xgb.XGBClassifier(tree_method="hist", learning_rate=0.01, n_estimators=200, max_depth=3, min_child_weight=1, objective="multi:softmax")
     model.fit(X_train, y_train_le)
 
     # Evaluate model performance using the features from the test set (X_test)
     y_pred = model.predict(X_test)
+    y_pred_train = model.predict(X_train)
 
     # Compare predictions (y_pred) with the labels in the test set (y_test)
     metrics = classification_report(y_test, le.inverse_transform(y_pred), output_dict=True)
-    print(f"Accuracy: {metrics['accuracy']}")
+    metrics_train = classification_report(y_train, le.inverse_transform(y_pred_train), output_dict=True)
+    print(f"Accuracy Test: {metrics['accuracy']}")
+    print(f"Accuracy Train: {metrics_train['accuracy']}")
     
     # Create the confusion matrix as a figure
-    results = confusion_matrix(y_test, le.inverse_transform(y_pred),labels=['Bearish', 'Bullish'])
-    df_cm = pd.DataFrame(results, ['True Bearish', 'True Bullish'],
-                            ['Pred Bearish', 'Pred Bullish'])
+    results = confusion_matrix(y_test, le.inverse_transform(y_pred),labels=['Bearish', 'Bullish', 'Neutral'])
+    df_cm = pd.DataFrame(results, ['True Bearish', 'True Bullish', 'True Neutral'],
+                            ['Pred Bearish', 'Pred Bullish', 'Pred Neutral'])
     cm = sns.heatmap(df_cm, annot=True, fmt='g')
     fig = cm.get_figure()
 
